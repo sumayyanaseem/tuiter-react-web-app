549706b9dcb69a1a33af7104a50bebe413d472e4
SUMAYYA NASEEM <sumayyanaseem@SUMAYYAs-MacBook-Air.local>
2022-09-08 06:00:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import neu from './pivot.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={neu} className="App-logo" alt="logo" />
+        <p>
+          Pivot Pivot Pivot...
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
